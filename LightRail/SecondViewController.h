65e490119409ee2075123e5dd3353d3b21b1b8eb
bjisa <benjisa@gmail.com>
2014-11-19 13:38:24 -0700

    Initial Commit

---
+++ LightRail/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  LightRail
+//
+//  Created by Ben Jisa on 11/19/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ LightRail/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LightRail
+//
+//  Created by Ben Jisa on 11/19/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
