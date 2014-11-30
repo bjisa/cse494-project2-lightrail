0bbcefe81263f9d6d4da6a4839a4b13099d8fd55
Amy Baldwin <amylynnb3@gmail.com>
2014-11-30 12:43:37 -0700

    Add view for station times tables

---
+++ LightRail/StationTimeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StationTimeTableViewController.h
+//  LightRail
+//
+//  Created by Amy Baldwin on 11/30/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationTimeTableViewController : UIViewController
+
+@end
---
+++ LightRail/StationTimeTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  StationTimeTableViewController.m
+//  LightRail
+//
+//  Created by Amy Baldwin on 11/30/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import "StationTimeTableViewController.h"
+
+@implementation StationTimeTableViewController
+
+@end
