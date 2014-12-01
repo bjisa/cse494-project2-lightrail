32bc3edba728d057283918beb9cf1bf4cee34223
Joshua Baldwin <jbaldwi6@asu.edu>
2014-11-30 17:26:38 -0700

    More code modfications

---
+++ LightRail/TripAnalyzer.h
@@ -0,0 +1,13 @@
+//
+//  TripAnalyzer.h
+//  LightRail
+//
+//  Created by Joshua Baldwin on 11/30/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TripAnalyzer : NSObject
+
+@end
---
+++ LightRail/TripAnalyzer.m
@@ -0,0 +1,13 @@
+//
+//  TripAnalyzer.m
+//  LightRail
+//
+//  Created by Joshua Baldwin on 11/30/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import "TripAnalyzer.h"
+
+@implementation TripAnalyzer
+
+@end
