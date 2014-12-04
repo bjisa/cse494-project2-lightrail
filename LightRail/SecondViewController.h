ba3c01f75871c4bb0f7cafe000e779b305f4fb0a
bjisa <benjisa@gmail.com>
2014-12-03 19:26:00 -0700

    Adding Maps tab

--- LightRail/SecondViewController.h
+++ LightRail/SecondViewController.h
@@ -7,9 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "StationModel.h"
+#import "Stations.h"
+@import CoreLocation;
+@import MapKit;
 
 @interface SecondViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) MKPolyline *trainRoute;
+@property (nonatomic, strong) MKPolylineView *polylineView;
+@property (nonatomic, retain) CLLocationManager* locationManager;
+@property (nonatomic, retain) CLLocation* initialLocation;
+@property (nonatomic, strong) NSString* direction;
+@property (nonatomic) int numOfEastboundStops;
 
 @end
-
