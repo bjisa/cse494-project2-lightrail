adc1a0a756a93bde745632ecbc4eb288698a78e1
bjisa <benjisa@gmail.com>
2014-12-03 21:14:07 -0700

    Add getNextTrip method to FavoritesViewController

--- LightRail/FavoritesViewController.m
+++ LightRail/FavoritesViewController.m
@@ -10,6 +10,7 @@
 #import "FavoriteStationTableCell.h"
 #import "Stations.h"
 #import "StationDetailsViewController.h"
+#import "Trip.h"
 
 @interface FavoritesViewController ()
 
@@ -97,7 +98,15 @@
     cell.stationName.text = station.name;
     
     // Calculate time remaining until next train arrives.
-    cell.timeRemaining.text = @"15 minutes";
+    //cell.timeRemaining.text = @"15 minutes";
+    
+    Trip *nextEastTrip = [self getNextTrip:[station.stopIDEastbound doubleValue]];
+    Trip *nextWestTrip = [self getNextTrip:[station.stopIDWestbound doubleValue]];
+    
+    NSString *eastTime = [self formatTimeString:nextEastTrip.departureTime];
+    NSString *westTime = [self formatTimeString:nextWestTrip.departureTime];
+    
+    cell.timeRemaining.text = eastTime;
 
     return cell;
 }
@@ -186,5 +195,183 @@
         [unarchiver finishDecoding];
     }
 }
+
+
+
+
+
+
+- (Trip *) getNextTrip:(double)stopID {
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"stop_times" ofType:@"txt"];
+    NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    
+    NSArray *items = [content componentsSeparatedByString:@"\n"];
+    
+    double tripID = 0;
+    
+    BOOL foundTripID = NO;
+    
+    // Figure out what the tripID is
+    
+    
+    for (NSString *item in items)
+    {
+        if (!foundTripID) {
+            if (!([[item stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+                   isEqualToString:@""] ||
+                  [[item stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+                   isEqualToString:@"stop_id_west,stop_id_east,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,wheelchair_boarding"]))
+            {
+                //NSLog(@"item = %@", item);
+                
+                NSArray *station = [item componentsSeparatedByString:@","];
+                
+                if (stopID == [station[3] doubleValue] && [self compareToCurrentTime:station[2]] == 1) {
+                    
+                    tripID = [station[0] doubleValue];
+                    
+                    foundTripID = YES;
+                }
+                
+            }
+        }
+    }
+    
+    // The tripID is the tripID of first element in stopsList
+    
+    
+    for (NSString *item in items)
+    {
+        
+        if (!([[item stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+               isEqualToString:@""] ||
+              [[item stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+               isEqualToString:@"stop_id_west,stop_id_east,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,wheelchair_boarding"]))
+        {
+            //NSLog(@"item = %@", item);
+            
+            NSArray *station = [item componentsSeparatedByString:@","];
+            NSMutableDictionary *stationdict = [NSMutableDictionary new];
+            
+            if (tripID == [station[0] doubleValue] && [self compareToCurrentTime:station[2]] == 1 && stopID == [station[3] doubleValue]) {
+                [stationdict setObject:station[0] forKey:@"trip_id"];
+                [stationdict setObject:station[2] forKey:@"departure_time"];
+                [stationdict setObject:station[3] forKey:@"stop_id"];
+                [stationdict setObject:station[6] forKey:@"stop_sequence"];
+                
+                Trip * newTrip = [[Trip alloc] initWithCSVDictionary:stationdict];
+                return newTrip;
+            }
+            
+        }
+    }
+    
+    return nil;
+    
+}
+
+- (int) compareToCurrentTime:(NSString *)trainTime
+{
+    // Get current time
+    NSDate *today = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"HH:mm:ss"];    // Capitalized "HH" gets the current time in the 24-hour format that we want here
+    NSString *currentTime = [dateFormatter stringFromDate:today];
+    //NSLog(@"Current Time: %@", currentTime);
+    
+    // Remove possible whitespace from trainTime
+    trainTime = [trainTime stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    
+    // Split times into arrays
+    NSArray *train = [trainTime componentsSeparatedByString:@":"];
+    NSArray *current = [currentTime componentsSeparatedByString:@":"];
+    
+    // Comparison Step #1: Compare the hours
+    if ([train[0] intValue] > [current[0] intValue])
+    {
+        return 1;
+    }
+    else if ([train[0] intValue] < [current[0] intValue])
+    {
+        return -1;
+    }
+    // Comparison Step #2: Compare the minutes
+    if ([train[1] intValue] > [current[1] intValue])
+    {
+        return 1;
+    }
+    else if ([train[1] intValue] < [current[1] intValue])
+    {
+        return -1;
+    }
+    // Comparison Step #3: Compare the seconds
+    if ([train[2] intValue] > [current[2] intValue])
+    {
+        return 1;
+    }
+    else if ([train[2] intValue] < [current[2] intValue])
+    {
+        return -1;
+    }
+    
+    // Train time and current time are equal
+    return 0;
+}
+
+// Format the time string into either 12 hour or 24 hour time format depending on the user's preferences
+- (NSString *) formatTimeString:(NSString *)str
+{
+    // Determine if the user wants a 12 hour clock or a 24 hour clock
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setLocale:[NSLocale currentLocale]];
+    [formatter setDateStyle:NSDateFormatterNoStyle];
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    NSString *dateString = [formatter stringFromDate:[NSDate date]];
+    NSRange amRange = [dateString rangeOfString:[formatter AMSymbol]];
+    NSRange pmRange = [dateString rangeOfString:[formatter PMSymbol]];
+    Boolean is24hour = (amRange.location == NSNotFound && pmRange.location == NSNotFound);
+    //NSLog(@"User wants a 24 hour clock? %@",(is24hour ? @"YES" : @"NO"));
+    
+    // Return the correct time style
+    if (is24hour)
+    {
+        return str;
+    }
+    else
+    {
+        NSArray *train = [str componentsSeparatedByString:@":"];
+        int hour = [train[0] intValue];
+        int minute = [train[1] intValue];
+        int second = [train[2] intValue];
+        if (hour > 12)          // PM
+        {
+            hour = hour % 12;
+            return [NSString stringWithFormat:@"%i:%02i:%02i PM", hour, minute, second];
+        }
+        else if (hour == 0)     // AM right after midnight
+        {
+            hour = 12;
+            return [NSString stringWithFormat:@"%i:%02i:%02i AM", hour, minute, second];
+        }
+        else if (hour == 12)    // PM at about noonish
+        {
+            return [NSString stringWithFormat:@"%i:%02i:%02i PM", hour, minute, second];
+        }
+        else                    // AM
+        {
+            return [NSString stringWithFormat:@"%i:%02i:%02i AM", hour, minute, second];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 @end
