cc6ab7a95cf52e3f86faf7c7a17ec566d0612789
Joshua Baldwin <jbaldwi6@asu.edu>
2014-12-10 10:59:20 -0700

    Update FavoriteStationTableCell.m
    Remove seconds from time when table cell loads or is modified

--- LightRail/FavoriteStationTableCell.m
+++ LightRail/FavoriteStationTableCell.m
@@ -14,27 +14,46 @@
 @synthesize nextTimeLabel = _nextTimeLabel;
 @synthesize directionSelector = _directionSelector;
 
-- (void)awakeFromNib {
+- (void)awakeFromNib 
+{
     // Initialization code
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated 
+{
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+    [self setTimeLabel];  // verify that time label is correct
 }
 
-- (IBAction)changeDirection:(id)sender 
+- (IBAction)changeDirection:(id)sender
 {
-    if (self.directionSelector.selectedSegmentIndex == 0)
+    [self setTimeLabel];
+}
+
+- (void) setTimeLabel
+{
+    @try
     {
-        self.nextTimeLabel.text = [[self.nextEastTime substringToIndex:[self.nextEastTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextEastTime rangeOfString:@" "].location]];
-        //NSLog(@"Next East time = %@", self.nextEastTime);
+        if (self.directionSelector.selectedSegmentIndex == 0)
+        {
+            self.nextTimeLabel.text = [[self.nextEastTime substringToIndex:[self.nextEastTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextEastTime rangeOfString:@" "].location]];
+            //NSLog(@"Next East time = %@", self.nextEastTime);
+        }
+        else
+        {
+            self.nextTimeLabel.text = [[self.nextWestTime substringToIndex:[self.nextWestTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextWestTime rangeOfString:@" "].location]];
+            //NSLog(@"Next West time = %@", self.nextWestTime);
+        }
     }
-    else
+    @catch (NSException *exception)
     {
-        self.nextTimeLabel.text = [[self.nextWestTime substringToIndex:[self.nextWestTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextWestTime rangeOfString:@" "].location]];
-        //NSLog(@"Next West time = %@", self.nextWestTime);
+        ;
+    }
+    @finally
+    {
+        ;
     }
 }
 
