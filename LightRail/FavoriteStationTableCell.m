a303f3a4bfd1845469f0af370c467ad96fd25b7d
Joshua Baldwin <jbaldwi6@asu.edu>
2014-12-10 10:33:38 -0700

    Update FavoriteStationTableCell.m
    Removed seconds from time string in cell

--- LightRail/FavoriteStationTableCell.m
+++ LightRail/FavoriteStationTableCell.m
@@ -24,11 +24,17 @@
     // Configure the view for the selected state
 }
 
-- (IBAction)changeDirection:(id)sender {
-    if (self.directionSelector.selectedSegmentIndex == 0) {
-        self.nextTimeLabel.text = self.nextEastTime;
-    } else {
-        self.nextTimeLabel.text = self.nextWestTime;
+- (IBAction)changeDirection:(id)sender 
+{
+    if (self.directionSelector.selectedSegmentIndex == 0)
+    {
+        self.nextTimeLabel.text = [[self.nextEastTime substringToIndex:[self.nextEastTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextEastTime rangeOfString:@" "].location]];
+        //NSLog(@"Next East time = %@", self.nextEastTime);
+    }
+    else
+    {
+        self.nextTimeLabel.text = [[self.nextWestTime substringToIndex:[self.nextWestTime rangeOfString:@":" options:NSBackwardsSearch].location] stringByAppendingString:[self.nextEastTime substringFromIndex:[self.nextWestTime rangeOfString:@" "].location]];
+        //NSLog(@"Next West time = %@", self.nextWestTime);
     }
 }
 
